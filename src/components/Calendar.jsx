e1c07d31047e1e8c407345a3b6cfcf3f1d2d6325
Hariom Mandal <hariommandal15@gmail.com>
2026-04-08 13:33:39 +0530

    Complete project

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -6,8 +6,10 @@ import { HeadName } from './HeadName'
 import { Notes } from './Notes'
 
 export const Calendar = () => {
+  const daysInMonth = 31; 
+
   return <div className='flex justify-center items-center mt-10'>
-    <div className=' border border-gray-400 shadow-2xl rounded-md w-lg h-150 flex flex-col p-3'>
+    <div className=' border border-gray-400 shadow-xl/30 rounded-md w-lg h-160 flex flex-col p-3 '>
     <div>
     <img src={imageAnime} alt="image" className='w-full object-cover rounded-sm'/>
     </div>
@@ -15,9 +17,22 @@ export const Calendar = () => {
     <Notes />
     <div className='w-full'>
     <HeadName text={"Date"}/>
-    <HeadName text={"2022 January"}/>
-    <div className='grid grid-cols-7'>
+    <HeadName text={"January 2022"}/>
+    <div className="grid grid-cols-7 text-center text-sm font-display text-gray-500">
+    {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((d) => (
+      <div key={d}>{d}</div>
+      ))}
     </div>
+    <div className="grid grid-cols-7 mt-2 text-center">
+  {Array.from({ length: daysInMonth }, (_, i) => i + 1).map((day) => (
+    <div
+      key={day}
+      className="p-2 rounded-md hover:bg-gray-200 cursor-pointer"
+    >
+      {day}
+    </div>
+  ))}
+</div>
     </div>
     </div>
     </div>
--- src/components/NotesText.jsx
+++ src/components/NotesText.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const NotesText = () => {
-  return <input type="text" className='border-b w-full outline-none underline-offset-1 mt-1 mb-0 font-display text-sm' />
+  return <input type="text" className='border-b w-full outline-none underline-offset-1 mt-1 mb-0 font-display text-sm border-gray-400' />
 }
